4c889b213694049c46c72dcb727e758dd17ace93
Glitch (tasteful-sage) <none>
2018-10-14 19:45:39 +0000

    🎑🚜 Checkpoint ./app.js:1474764/262

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const mongo = require("mongodb");
 const mongoose = require("mongoose");
 
+//create schema for db structure
 const schema = new mongoose.Schema({
   originalURL: {type: String, required: true},
   shortURL: String
 })
 
+
+// use the schema to make a Site object to update/use
+const Site = mongoose.model("Site", schema)
+
+//function to update the db with new website
+
+const addShortURL = function(original, done) {
+  
+}
