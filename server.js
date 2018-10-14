8641bd61a777136068c3f362c4f1af7a76e42d3c
Glitch (tasteful-sage) <none>
2018-10-14 20:58:34 +0000

    ✨🕌 Checkpoint ./server.js:1474764/1278

--- server.js
+++ server.js
@@ -24,22 +24,31 @@ const url = process.env.MONGO_URI;
 MongoClient.connect(url, function(err, db) {
   if (err) console.log(err);
   console.log("Database created!");
-  
-  app.use('/public', express.static(process.cwd() + '/public'));
+  // db.createCollection('websites', (err, res) => {
+  //   if (err) console.log(err);
+  //   console.log("Collection created!");
+  //   db.close();
+  // })
+});
 
-  app.get('/', function(req, res){
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.use('/public', express.static(process.cwd() + '/public'));
 
-  // your first API endpoint... 
-  app.get("/api/hello", function (req, res) {
-    res.json({greeting: 'hello API'});
-  });
+app.get('/', function(req, res){
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+  //testing here
+  const testObj = {original: "https://www.google.com", short:"goo"}
+  MongoClient.connect(url, (err, db) => {
+    db.collection("websites", 
+  })
+});
 
-  app.listen(port, function () {
-    console.log('Node.js listening ...');
-  });
-  
-  // db.close();
+//create a post here, where the connection is opened (https://www.w3schools.com/nodejs/nodejs_mongodb_createcollection.asp) and edited
+
+app.listen(port, function () {
+  console.log('Node.js listening ...');
 });
